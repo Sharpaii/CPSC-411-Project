0bc30359fb1366d2573611b0c907cb675c354e46
Madeline Sharpe <50880124+Sharpaii@users.noreply.github.com>
2023-10-19 16:33:02 -0700

    Add files via upload

---
+++ ScheduleApp/ScheduleApp/ScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleAppApp.swift
+//  ScheduleApp
+//
+//  Created by Madeline Sharpe on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
